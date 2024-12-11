6329bc0d38fe175bd047b52ee4afe03e027d9194
Alexandre Daby-Seesaram <alexandre.daby-seesaram@polytechnique.edu>
2024-12-11 20:34:00 +0100

    Initial commit

---
+++ Create_data.py
@@ -0,0 +1,137 @@
+
+import numpy as np
+import os
+
+from create_vti_from_scans import get_metada_PGM, pgm2array, array2vti, get_z_metadata_flatten_image
+
+
+N_patients = 9
+Patients_Ids = list(range(1,  N_patients + 1))
+# Patients_Ids.remove(1)
+
+# Patients_Ids = [1]
+
+initial_path = '/Users/daby/LargeFiles/Geometries_alexandre/Patient_data/Images/'
+
+destination_path = "./"
+
+RAW_vti = False
+Vti_Binary_PGM = False # check +- 1 on pixels
+Vti_RAW_PGM = False
+Blur_binary_vti = False
+Clean_BINARY_PGM = False
+Copy_initial_image = True
+
+
+
+if RAW_vti:
+    for i in Patients_Ids:
+        prefix = "PA"+str(i)
+        folder = prefix+"_images/"
+        file_init = Folder+prefix+"_M0_RAW_1.vti"
+
+        new_folder = destination_path+prefix
+
+        new_file = new_folder+ "/Image_01.vti"
+        new_file_ref = new_folder+ "/Image_00.vti"
+
+        command_mkdir = "mkdir "+new_folder
+        # print(command_mkdir)
+        # os.system(command_mkdir)
+        command_cp = "cp "+initial_path+file_init+" "+new_file
+        # print(command_cp)
+        os.system(command_cp)
+        command_cp_ref =  "cp E2/Image_P_00.vti "+new_file_ref
+        os.system(command_cp_ref)
+
+
+
+original_dir = os.getcwd()
+
+
+if Clean_BINARY_PGM:
+    for i in Patients_Ids:
+        prefix = "PA"+str(i)
+        # Gif
+        post_gif = prefix+"/LUNG/"+"Pat"+str(i)+"_inspi?_????_colorcls.gif"
+        command_del = "rm "+post_gif
+        os.system(command_del)
+
+        # textured binary
+        post_textured = prefix+"/LUNG/"+"Pat"+str(i)+"_inspi?_????_predcls.pgm"
+        command_del = "rm "+post_textured
+        os.system(command_del)
+
+        os.chdir(prefix+"/LUNG")
+        for filename in os.listdir():
+            
+            if filename.endswith("_lung.pgm"):
+                # Construct new filename by removing "_lung"
+                new_filename = filename.replace("_lung", "")
+                # Rename the file
+                os.rename(filename, new_filename)
+                print(f"Renamed: {filename} -> {new_filename}")
+        os.chdir(original_dir)
+
+
+
+# Need to read metadata from raw images
+
+
+if Vti_Binary_PGM:
+    for i in Patients_Ids:
+        prefix = "PA"+str(i)
+        input_files = prefix+"/LUNG/"+"Pat"+str(i)+"_inspi1"
+        input_files_raw = prefix+"/PGM/"+"Pat"+str(i)+"_inspi1"             # For metadata only here
+        # print(os.system("pwd"))
+        # print(input_files)
+        output = prefix+"/"+prefix+"_Binary"
+        pgm_files, image_array = pgm2array(input_files)
+        pgm_files_raw, _ = pgm2array(input_files_raw)
+        metadata_fields = ["Slice_Location", "Pixel_Size"]
+        metadata = get_metada_PGM(
+                            input_file        = pgm_files_raw[0],
+                            metadata_fields   = metadata_fields
+                        )
+        image_shape, pixel_size, image_pos, flatten_image_array = get_z_metadata_flatten_image(pgm_files_raw, metadata, image_array)
+        array2vti(
+                image_shape = image_shape,
+                pixel_size  = pixel_size,
+                image_pos   = image_pos,
+                input_array = flatten_image_array,
+                field_name  = 'pixel intensity',
+                output      = output)
+        print("Patient "+prefix+"done")
+
+
+
+if Blur_binary_vti:
+    import vtk 
+
+    for i in Patients_Ids:
+        prefix = "PA"+str(i)
+        input_file = prefix+"/"+prefix+"_Binary.vti"
+        # Step 1: Read the .vti file
+        reader = vtk.vtkXMLImageDataReader()
+        reader.SetFileName(input_file)
+        reader.Update()
+
+        # Step 2: Apply Gaussian smoothing
+        gaussian = vtk.vtkImageGaussianSmooth()
+        gaussian.SetInputConnection(reader.GetOutputPort())
+        gaussian.SetStandardDeviations(5.0, 5.0, 5.0)                           # Standard deviations for the Gaussian in X, Y, Z
+        gaussian.SetRadiusFactors(10.0, 10.0, 10.0)                                # Radius factors 
+        gaussian.Update()
+
+        # Step 3: Write the output to a new .vti file
+        writer = vtk.vtkXMLImageDataWriter()
+        writer.SetFileName(prefix+"/Image_Binary_blurred_01.vti")
+        writer.SetInputConnection(gaussian.GetOutputPort())
+        writer.Write()
+
+if Copy_initial_image:
+
+    for i in Patients_Ids:
+        prefix = "PA"+str(i)
+        cp_command_images = "cp PA1/Image_Binary_blurred_01.vti "+prefix+"/Image_Binary_blurred_00.vti"
+        os.system(cp_command_images)
---
+++ Lungs_geometry_generate_mapping.py
@@ -0,0 +1,145 @@
+
+import numpy as np
+import dolfin
+import meshio
+import vtk
+import fenics as fenics
+import dolfin_warp     as dwarp
+import dolfin_mech as dmech
+import myVTKPythonLibrary as myvtk
+
+#%% Define initial mesh (that hold all mappings)
+
+mesh_LL=fenics.Mesh("Initial_Config/mesh_LL.xml")
+mesh_RL=fenics.Mesh("Initial_Config/mesh_RL.xml")
+
+
+
+regul_level = 0.0
+regul_type = "discrete-equilibrated-tractions-normal"
+
+if any([_ in regul_type for _ in ["linear", "simple"]]):
+    regul_model = "hooke"
+else:
+    regul_model = "ciarletgeymonatneohookean"
+
+
+
+
+#%% Create initial scaling 
+# The objective is to create an initial scaled mapping that lies within all possible rib cages to help with tracking
+def initial_scaling(mesh, lung, coef =-0.4):
+    saving_name_initial_scalaing = "initial_scaling_"+lung+".dat"
+    alpha = coef                                                                      # Scaling factor
+
+    dim = 3
+    x = dolfin.SpatialCoordinate(mesh)
+    dV = dolfin.Measure(
+            "dx",
+            domain=mesh)
+
+    center_gravity = [0,0,0]                                                            # Center of mass coordinates
+    for k_dim in range(dim):
+        center_gravity[k_dim] = dolfin.assemble(x[k_dim]*dV)/dolfin.assemble(1*dV)
+
+    x_mid = center_gravity[0]
+    y_mid = center_gravity[1]
+    z_mid = center_gravity[2]
+
+    ## Write down reduced_kinematics initialisation file
+    reduced_disp_initial_scaling = alpha*np.array([[-x_mid,-y_mid,-z_mid,1,1,1]])       # Reduced displacement for the 6 modes reduced-kinematics
+    np.savetxt(saving_name_initial_scalaing, reduced_disp_initial_scaling)                     # Save the reduced displacements
+
+#%% Define tracking functions
+
+destination_path = "./"                                                             #Root path for patients solution folders
+
+def reduced_kiematics(patient,lung, mesh, tol=1e-6):
+    prefix = "PA"+str(patient)
+    dwarp.warp(
+            working_folder = destination_path+prefix,
+            working_basename = "mapping_reduced_kinematics"+'_'+prefix+'_'+lung,
+            images_folder = destination_path+prefix,
+            images_basename = "Image_Binary_blurred",
+            images_ext = "vti",
+            mesh=mesh,
+            images_quadrature = 3,
+            n_iter_max = 500,
+            regul_poisson = 0.3,
+            tangent_type = "Idef",
+            nonlinearsolver = "reduced_kinematic_newton",
+            regul_type=regul_type,
+            regul_model=regul_model,
+            regul_level=regul_level,
+            relax_type="constant",
+            tol_dU=tol,
+            continue_after_fail = 1,
+            write_VTU_files=True,
+            write_VTU_files_with_preserved_connectivity=True,
+            initialize_reduced_U_from_file = True,
+            initialize_reduced_U_filename = "initial_scaling_"+lung+".dat",
+            print_iterations=1) 
+
+
+def tracking(patient,lung, mesh, tol=1e-3):
+    prefix = "PA"+str(patient)
+    dwarp.warp(
+            working_folder = destination_path+prefix,
+            working_basename = "mapping"+'_'+prefix+'_'+lung,
+            images_folder = destination_path+prefix,
+            images_basename = "Image_Binary_blurred",
+            images_ext = "vti",
+            mesh=mesh,
+            n_iter_max = 1000,
+            tangent_type = "Idef",
+            nonlinearsolver = "newton",
+            regul_types = [ "continuous-hyperelastic"], #"discrete-mesh",  "continuous-equilibrated", "discrete-tractions-normal", "continuous-equilibrated", "discrete-tractions-tangential", "continuous-hyperelastic" "continuous-hyperelastic",
+            regul_model = "ogdenciarletgeymonatneohookeanmooneyrivlin",
+            # regul_level = 0.0001,
+            regul_levels = [0.1],
+            regul_poisson = 0.3,
+            images_quadrature = 6,
+            images_quadrature_from = "points_count",
+            relax_type = "backtracking",
+            tol_dU=tol,
+            write_VTU_files=True,
+            write_VTU_files_with_preserved_connectivity=True,
+            images_char_func = True,
+            initialize_U_from_file = 1,
+            initialize_U_folder = destination_path+prefix, 
+            initialize_U_basename = "mapping_reduced_kinematics"+'_'+prefix+'_'+lung,
+            initialize_U_ext = "vtu",
+            initialize_U_array_name = "displacement",
+            initialize_U_method = "dofs_transfer", # dofs_transfer, interpolation, projection
+            print_iterations=1) # dofs_transfer, interpolation, projection)
+
+
+N_patients = 9
+Patients_Ids = list(range(1,  N_patients + 1))
+Patients_Ids.remove(1)
+
+Lungs = ['RL','LL']
+Lungs = ['RL']
+
+
+# Patients_Ids = [1]
+
+for lung in Lungs:
+    match lung:
+        case 'LL':
+            mesh = mesh_LL
+        case 'RL':
+            mesh = mesh_RL
+    initial_scaling(mesh, lung, coef =-0.2)
+    for patient in Patients_Ids:
+        reduced_kiematics(
+                patient = patient,
+                lung = lung,
+                mesh = mesh,
+                tol=1e-6)
+
+        tracking(
+                patient = patient,
+                lung = lung,
+                mesh = mesh,
+                tol=1e-6)
