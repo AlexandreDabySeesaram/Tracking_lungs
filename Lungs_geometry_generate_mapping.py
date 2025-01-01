daf28b72a7c1aedd48735a1845f7faf8b2eff301
Alexandre Daby-Seesaram <alexandre.daby-seesaram@polytechnique.edu>
2025-01-01 16:16:14 +0100

    Start using dwarp multi-blur function

--- Lungs_geometry_generate_mapping.py
+++ Lungs_geometry_generate_mapping.py
@@ -92,7 +92,8 @@ def reduced_kiematics(image_base_name, patient,lung, mesh, tol=1e-6, images_quad
             write_VTU_files_with_preserved_connectivity = True,
             initialize_reduced_U_from_file              = True,
             initialize_reduced_U_filename               = "initial_scaling_"+lung+".dat",
-            print_iterations                            =1) 
+            print_iterations                            = 1,
+            save_reduced_disp                           = True) 
 
 
 def tracking(patient,lung, mesh, tol=1e-3, regul = 0.5, images_quadrature = 6):
@@ -122,7 +123,7 @@ def tracking(patient,lung, mesh, tol=1e-3, regul = 0.5, images_quadrature = 6):
             initialize_U_ext                            = "vtu",
             initialize_U_array_name                     = "displacement",
             initialize_U_method                         = "dofs_transfer",                      # dofs_transfer, interpolation, projection
-            print_iterations                            = 1)                                    # dofs_transfer, interpolation, projection)
+            print_iterations                            = 1)                                    
 
 
 
@@ -146,43 +147,29 @@ def warp_and_blur(patient, attenuation_factors, lung, mesh):
         ### Update with new reduced kinematics: Check that results of reduced kinematics is saved in .dat
 
         if i >=1 :
-            dwarp.warp(
-                images_char_func                = False,
-                working_folder                  = working_folder,
-                working_basename                = working_basename+"_downsampled="+str(attenuation_factor),
-                images_folder                   = images_folder,
-                images_basename                 = images_basename_blur_factor,
-                images_quadrature               = images_quadrature_progressive[i],
-                mesh                            = mesh,
-                kinematics_type                 = kinematics_type,
-                reduced_kinematics_model        = reduced_kinematics_model,
-                normalize_energies              = normalize_energies,
-                relax_type                      = relax_type,
-                tol_dU                          = tol_dU,
-                write_qois_limited_precision    = write_qois_limited_precision, 
-                initialize_reduced_U_from_file  = initialize_reduced_U_from_file,
-                initialize_reduced_U_filename   = initialize_reduced_U_filename,
-                )
+            initialize_reduced_U_filename = self.working_basename+"_reduced_kinematics_inter.dat"
 
                 ###### Check that .dat is saved in reduced kinematics for latter initialisation check name for initial .dat files
         else:
-            dwarp.warp(
-                images_char_func                = False,
-                working_basename                = working_basename+"_downsampled="+str(attenuation_factor),
-                images_folder                   = images_folder,
-                images_basename                 = images_basename_blur_factor,
-                images_quadrature               = images_quadrature_progressive[i],
-                mesh                            = mesh,
-                kinematics_type                 = kinematics_type,
-                reduced_kinematics_model        = reduced_kinematics_model,
-                normalize_energies              = normalize_energies,
-                relax_type                      = relax_type,
-                tol_dU                          = tol_dU,
-                write_qois_limited_precision    = write_qois_limited_precision, 
-                initialize_reduced_U_from_file  = True,
-                initialize_reduced_U_filename   = working_basename+"_downsampled="+str(attenuation_factor)+".dat",
-                )
+            initialize_reduced_U_filename = "initial_scaling_"+lung+".dat"
 
+        dwarp.warp(
+            images_char_func                = False,
+            working_folder                  = working_folder,
+            working_basename                = working_basename+"_downsampled="+str(attenuation_factor),
+            images_folder                   = images_folder,
+            images_basename                 = images_basename_blur_factor,
+            images_quadrature               = images_quadrature_progressive[i],
+            mesh                            = mesh,
+            kinematics_type                 = kinematics_type,
+            reduced_kinematics_model        = reduced_kinematics_model,
+            normalize_energies              = normalize_energies,
+            relax_type                      = relax_type,
+            tol_dU                          = tol_dU,
+            write_qois_limited_precision    = write_qois_limited_precision, 
+            initialize_reduced_U_from_file  = initialize_reduced_U_from_file,
+            initialize_reduced_U_filename   = initialize_reduced_U_filename,
+            )
 
 
 
@@ -209,6 +196,39 @@ Lungs = ['RL']
 reduced_kinematics_model = "translation+scaling+rotation"
 Patients_Ids = [5]
 
+# for lung in Lungs:
+#     match lung:
+#         case 'LL':
+#             mesh = mesh_LL
+#         case 'RL':
+#             mesh = mesh_RL
+#     initial_scaling(mesh, lung, coef =0.9, reduced_kinematics_model = reduced_kinematics_model)
+#     for patient in Patients_Ids:
+#         reduced_kiematics(
+#                 image_base_name                         = "_INT_thrshld_external_gradient_blurred",
+#                 patient                                 = patient,
+#                 lung                                    = lung,
+#                 mesh                                    = mesh,
+#                 tol                                     = 1e-6,
+#                 images_quadrature                       = 3,
+#                 reduced_kinematics_model                = reduced_kinematics_model
+#                 )
+
+        # tracking(
+        #         patient                                 = patient,
+        #         lung                                    = lung,
+        #         mesh                                    = mesh,
+        #         tol                                     = 1e-4,
+        #         regul                                   = 0.01)
+
+
+
+#%% Test of blur and warp from dwarp directly
+
+image_base_name = "_INT_thrshld_external_gradient_blurred"
+reduced_kinematics_model = "translation+scaling+rotation"
+
+
 for lung in Lungs:
     match lung:
         case 'LL':
@@ -217,19 +237,32 @@ for lung in Lungs:
             mesh = mesh_RL
     initial_scaling(mesh, lung, coef =0.9, reduced_kinematics_model = reduced_kinematics_model)
     for patient in Patients_Ids:
-        reduced_kiematics(
-                image_base_name                         = "_INT_thrshld_external_gradient_blurred",
-                patient                                 = patient,
-                lung                                    = lung,
-                mesh                                    = mesh,
-                tol                                     = 1e-6,
-                images_quadrature                       = 3,
-                reduced_kinematics_model                = reduced_kinematics_model
-                )
-
-        # tracking(
-        #         patient                                 = patient,
-        #         lung                                    = lung,
-        #         mesh                                    = mesh,
-        #         tol                                     = 1e-4,
-        #         regul                                   = 0.01)
+        prefix = "PA"+str(patient)
+        
+        dwarp.blur_and_warp(
+                    attenuation_factors                         = [2,4,8],
+                    images_char_func                            = False,
+                    working_folder                              = destination_path+prefix,
+                    # working_basename                            = "thrshd_mapping_reduced_kinematics"+'_'+prefix+'_'+lung,
+                    working_basename                            = "MB_mapping_reduced_kinematics"+'_'+prefix+'_'+lung,
+                    images_folder                               = destination_path+prefix,
+                    images_basename                             = prefix+image_base_name,
+                    images_ext                                  = "vti",
+                    mesh                                        = mesh,
+                    kinematics_type                             = "reduced",
+                    reduced_kinematics_model                    = reduced_kinematics_model,
+                    images_quadrature                           = 6,
+                    n_iter_max                                  = 10,
+                    regul_poisson                               = 0.3,
+                    regul_type                                  = regul_type,
+                    regul_model                                 = regul_model,
+                    regul_level                                 = regul_level,
+                    relax_type                                  ="backtracking",
+                    tol_dU                                      = 1e-3,
+                    continue_after_fail                         = 1,
+                    write_VTU_files                             = True,
+                    write_VTU_files_with_preserved_connectivity = True,
+                    initialize_reduced_U_from_file              = True,
+                    initialize_reduced_U_filename               = "initial_scaling_"+lung+".dat",
+                    print_iterations                            = 1,
+                    save_reduced_disp                           = True) 
